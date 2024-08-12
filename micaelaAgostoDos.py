746370d4fc15be3bb4c2bf06a8f66a82f3e7572c
Micaolmos <162600985+Micaolmos@users.noreply.github.com>
2024-08-12 12:19:20 -0300

    Add files via upload

---
+++ micaelaAgostoDos.py
@@ -0,0 +1,28 @@
+def menu():
+    menuInicio = '''
+    //////////////////////////////////////////
+    /     1 - Añadir una tarea               /
+    /     2 - Ver todas las tareas           /
+    /     3 -Marcar una tarea como completa  /
+    /     4 - Salir                          /
+    ////////////////////////////////////////// ''' 
+    print(menuInicio)
+opcion = int(input("Ingrese la opcion seleccionada: "))
+opcion = menu()
+
+listaTareas=[]
+diccionario = {}
+while True: 
+    menu()
+    if opcion == 1:
+        Tarea:int(input("Ingresa la nueva tarea:"))
+        print("Se guardo tu nueva tarea")
+
+    elif opcion == 2: 
+        print:listaTareas=[]
+
+
+    elif opcion == 4:
+       print("Adios, gracias por usar el codigo")
+    break
+listaTareas.append(Tarea)
